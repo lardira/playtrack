94d5002c4773e6d07c01de486c79bf46fc67eaa0
lardira <danilkefimov@gmail.com>
2026-01-25 19:22:10 +0300

    fix

--- internal/domain/player/handler.go
+++ internal/domain/player/handler.go
@@ -227,10 +227,14 @@ func (h *Handler) UpdatePlayedGame(
 		now := time.Now()
 		switch status {
 		case PlayedGameStatusDropped:
-			newPoints = -1
-
 			prevGame, err := h.playerRepository.FindLastPlayedGame(ctx, i.PlayerID)
-			if !errors.Is(err, ErrPlayedGameNotFound) {
+			if err != nil {
+				if errors.Is(err, ErrPlayedGameNotFound) {
+					newPoints = -1
+				} else {
+					return nil, huma.Error400BadRequest("game played find", err)
+				}
+			} else {
 				newPoints = prevGame.Points * 2
 			}
 
