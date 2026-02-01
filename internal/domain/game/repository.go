74a94b5eeef31f188fed5bb9961846b005604004
lardira <danilkefimov@gmail.com>
2026-02-01 18:04:51 +0300

    game

--- internal/domain/game/repository.go
+++ internal/domain/game/repository.go
@@ -4,7 +4,17 @@ import (
 	"context"
 	"errors"
 
+	sq "github.com/Masterminds/squirrel"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	TableGame = "game"
+)
+
+const (
+	gameColumns string = "id, points, hours_to_beat, title, url, created_at"
 )
 
 var (
@@ -24,95 +34,106 @@ func NewPGRepository(pool *pgxpool.Pool) *PGRepository {
 func (r *PGRepository) FindAll(ctx context.Context) ([]Game, error) {
 	out := make([]Game, 0)
 
-	query := `SELECT 
-				id, points, hours_to_beat, title, url, created_at  
-			FROM game`
-	rows, err := r.pool.Query(ctx, query)
+	sqlBuild := sq.Select(gameColumns).
+		PlaceholderFormat(sq.Dollar).
+		From(TableGame)
+
+	query, args, err := sqlBuild.ToSql()
+	if err != nil {
+		return nil, err
+	}
+	rows, err := r.pool.Query(ctx, query, args...)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
 	for rows.Next() {
-		var g Game
-		err := rows.Scan(
-			&g.ID,
-			&g.Points,
-			&g.HoursToBeat,
-			&g.Title,
-			&g.URL,
-			&g.CreatedAt,
-		)
+		g, err := gameFromRow(rows)
 		if err != nil {
 			return nil, err
 		}
-		out = append(out, g)
+		out = append(out, *g)
 	}
 	return out, nil
 }
 
 func (r *PGRepository) FindOne(ctx context.Context, id int) (*Game, error) {
-	var game Game
+	sqlBuild := sq.Select(gameColumns).
+		PlaceholderFormat(sq.Dollar).
+		From(TableGame).
+		Where(sq.Eq{"id": id})
 
-	query := `SELECT 
-				id, points, hours_to_beat, title, url, created_at  
-			FROM game 
-			WHERE id=$1`
-
-	row := r.pool.QueryRow(ctx, query, id)
-	err := row.Scan(
-		&game.ID,
-		&game.Points,
-		&game.HoursToBeat,
-		&game.Title,
-		&game.URL,
-		&game.CreatedAt,
-	)
+	query, args, err := sqlBuild.ToSql()
 	if err != nil {
 		return nil, err
 	}
-	return &game, nil
+
+	row := r.pool.QueryRow(ctx, query, args...)
+	g, err := gameFromRow(row)
+	if err != nil {
+		return nil, err
+	}
+	return g, nil
 }
 
 func (r *PGRepository) FindByTitle(ctx context.Context, title string) (*Game, error) {
-	var game Game
-
 	query := `SELECT 
 				id, points, hours_to_beat, title, url, created_at  
 			FROM game 
 			WHERE title=$1`
 
-	row := r.pool.QueryRow(ctx, query, title)
+	sqlBuild := sq.Select(gameColumns).
+		PlaceholderFormat(sq.Dollar).
+		From(TableGame).
+		Where(sq.Eq{"title": title})
+
+	query, args, err := sqlBuild.ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	row := r.pool.QueryRow(ctx, query, args...)
+	g, err := gameFromRow(row)
+	if err != nil {
+		return nil, err
+	}
+	return g, nil
+}
+
+func (r *PGRepository) Insert(ctx context.Context, game *Game) (int, error) {
+	var id int
+
+	sqlBuild := sq.Insert(TableGame).
+		PlaceholderFormat(sq.Dollar).
+		Columns("points", "hours_to_beat", "title", "url").
+		Values(game.Points, game.HoursToBeat, game.Title, game.URL).
+		Suffix("RETURNING id")
+
+	query, args, err := sqlBuild.ToSql()
+	if err != nil {
+		return id, err
+	}
+
+	row := r.pool.QueryRow(ctx, query, args...)
+	if err := row.Scan(&id); err != nil {
+		return id, err
+	}
+	return id, nil
+}
+
+func gameFromRow(row pgx.Row) (*Game, error) {
+	var g Game
 	err := row.Scan(
-		&game.ID,
-		&game.Points,
-		&game.HoursToBeat,
-		&game.Title,
-		&game.URL,
-		&game.CreatedAt,
+		&g.ID,
+		&g.Points,
+		&g.HoursToBeat,
+		&g.Title,
+		&g.URL,
+		&g.CreatedAt,
 	)
 	if err != nil {
 		return nil, err
 	}
-	return &game, nil
-}
-
-func (r *PGRepository) Insert(ctx context.Context, game *Game) (int, error) {
-	if found, _ := r.FindByTitle(ctx, game.Title); found != nil {
-		return 0, ErrFoundByTitle
-	}
-
-	var id int
-
-	query := `INSERT INTO 
-				game (points, hours_to_beat, title, url) 
-			VALUES ($1, $2, $3, $4)
-			RETURNING id`
-
-	row := r.pool.QueryRow(ctx, query, game.Points, game.HoursToBeat, game.Title, game.URL)
-	err := row.Scan(&id)
-	if err != nil {
-		return id, err
-	}
-	return id, nil
+	return &g, nil
 }
