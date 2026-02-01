202e302cdbb6e4909d80881731a7740cbe8f5bff
lardira <danilkefimov@gmail.com>
2026-02-01 22:40:18 +0300

    fix: add patch method to cors middleware

--- internal/server/server.go
+++ internal/server/server.go
@@ -46,7 +46,7 @@ func New(ctx context.Context, opts Options) (*Server, error) {
 	mux := http.NewServeMux()
 	servMux := cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
 		AllowedHeaders:   []string{"*"},
 		AllowCredentials: true,
 	}).Handler(mux)
